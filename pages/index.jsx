80b28be03707ba7c2df98e455e3741ec14a391e9
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2022-10-05 11:37:32 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,23 +6,23 @@ import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
   const [timecome1, settimecome1] = useState(23400);
-  const [timegehen1, settimegehen1] = useState(54000);
+  const [timegehen1, settimegehen1] = useState(53280);
   const [workhours1, setworkhours1] = useState(0);
 
   const [timecome2, settimecome2] = useState(23400);
-  const [timegehen2, settimegehen2] = useState(54000);
+  const [timegehen2, settimegehen2] = useState(53280);
   const [workhours2, setworkhours2] = useState(0);
 
   const [timecome3, settimecome3] = useState(23400);
-  const [timegehen3, settimegehen3] = useState(54000);
+  const [timegehen3, settimegehen3] = useState(53280);
   const [workhours3, setworkhours3] = useState(0);
 
   const [timecome4, settimecome4] = useState(23400);
-  const [timegehen4, settimegehen4] = useState(54000);
+  const [timegehen4, settimegehen4] = useState(53280);
   const [workhours4, setworkhours4] = useState(0);
 
   const [timecome5, settimecome5] = useState(23400);
-  const [timegehen5, settimegehen5] = useState(54000);
+  const [timegehen5, settimegehen5] = useState(53280);
   const [workhours5, setworkhours5] = useState(0);
 
   useEffect(() => {
@@ -47,8 +47,7 @@ export default function Home() {
   var wochenstunden = Math.round(((workhours1 - 7.8)+(workhours2 - 7.8)+(workhours3 - 7.8)+(workhours4 - 7.8)+(workhours5 - 7.8)) * 100) / 100;
   
 
-
-
+  
   return (
     <Container className="md-container">
       <Head>
@@ -70,7 +69,7 @@ export default function Home() {
             <TimePicker value={timecome1} onChange={el => settimecome1(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
             <TimePicker value={timegehen1} onChange={el => settimegehen1(el)} start="6:30" end="18:00" step="15" format={24} />
             <Card.Text>Stunden:<br />
-              <h4>{workhours1}</h4>
+              <h4>{workhours1.toFixed(2)}</h4>
               Überstunden: <h4>{Math.round((workhours1 - 7.8) * 100) / 100}</h4>
             </Card.Text>
             </Card.Body>
@@ -81,7 +80,7 @@ export default function Home() {
             <TimePicker value={timecome2} onChange={el => settimecome2(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
             <TimePicker value={timegehen2} onChange={el => settimegehen2(el)} start="6:30" end="18:00" step="15" format={24} />
             <Card.Text>Stunden:<br />
-              <h4>{workhours2}</h4>
+              <h4>{workhours2.toFixed(2)}</h4>
               Überstunden: <h4>{Math.round((workhours2 - 7.8) * 100) / 100}</h4>
             </Card.Text>
             </Card.Body>
@@ -92,7 +91,7 @@ export default function Home() {
             <TimePicker value={timecome3} onChange={el => settimecome3(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
             <TimePicker value={timegehen3} onChange={el => settimegehen3(el)} start="6:30" end="18:00" step="15" format={24} />
             <Card.Text>Stunden:<br />
-              <h4>{workhours3}</h4>
+              <h4>{workhours3.toFixed(2)}</h4>
               Überstunden: <h4>{Math.round((workhours3 - 7.8) * 100) / 100}</h4>
             </Card.Text>
             </Card.Body>
@@ -103,7 +102,7 @@ export default function Home() {
             <TimePicker value={timecome4} onChange={el => settimecome4(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
             <TimePicker value={timegehen4} onChange={el => settimegehen4(el)} start="6:30" end="18:00" step="15" format={24} />
             <Card.Text>Stunden:<br />
-              <h4>{workhours4}</h4>
+              <h4>{workhours4.toFixed(2)}</h4>
               Überstunden: <h4>{Math.round((workhours4 - 7.8) * 100) / 100}</h4>
             </Card.Text>
             </Card.Body>
@@ -114,7 +113,7 @@ export default function Home() {
             <TimePicker value={timecome5} onChange={el => settimecome5(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
             <TimePicker value={timegehen5} onChange={el => settimegehen5(el)} start="6:30" end="18:00" step="15" format={24} />
             <Card.Text>Stunden:<br />
-              <h4>{workhours5}</h4>
+              <h4>{workhours5.toFixed(2)}</h4>
               Überstunden: <h4>{Math.round((workhours5 - 7.8) * 100) / 100}</h4>
             </Card.Text>
             </Card.Body>
