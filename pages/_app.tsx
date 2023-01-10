f78717b7b0a1272e7ead67fea2dbeb236f8074d4
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2023-01-10 11:16:28 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
-import '../style/index.css'
+import { Analytics } from '@vercel/analytics/react';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
