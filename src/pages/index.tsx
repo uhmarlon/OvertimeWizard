e74483e46cb7471fe990d12c21635a7e6d7ca8c6
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2023-01-11 12:59:16 +0100

    mobiel fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,8 @@ export default function Home() {
     if(currentDay > 0 && currentDay < 6)
     {
       var card = document.getElementById(days[currentDay-1]);
-      card.setAttribute('class', 'card border-dark');
+      card.classList.remove("border-gray-200");
+      card.classList.add("border-gray-500");
     }
   })
 
@@ -70,117 +71,67 @@ export default function Home() {
       </Head>
 
 
-      <h1 className="mt-36 font-bold text-4xl md:text-5xl mb-4 text-center">Willkommen zum Stundenrechner der <br/>
+      <h1 className="mt-12 font-bold text-4xl md:text-5xl mb-4 text-center">Willkommen zum Stundenrechner der <br/>
         <Image className='object-center' src="/swmicon.svg" alt="SWM" width={240} height={80} />
       </h1>
 
-      <div className='grid grid-rows-2 grid-flow-col gap-4'>
-        <div className="row-span-2 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <div className='grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3 pt-2 pb-2 lg:pb-5'>
+        <div id='Montag' className="row-span-3 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Montag</h5>
+            <TimePicker value={timecome1} onChange={el => settimecome1(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
+            <TimePicker value={timegehen1} onChange={el => settimegehen1(el)} start="6:30" end="18:00" step="15" format={24} />
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Stunden: <b className='hourTextField'>{workhours1.toFixed(2)}</b></h6>
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Überstunden: <b className='hourTextField'>{Math.round((workhours1 - 7.8) * 100) / 100}</b></h6>
         </div>
 
-        <div className="row-span-2 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <div id='Dienstag' className="row-span-3 max-w-sm p-6  border border-b-8 border-gray-200 rounded-lg shadow-md">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Dienstag</h5>
+            <TimePicker value={timecome2} onChange={el => settimecome2(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
+            <TimePicker value={timegehen2} onChange={el => settimegehen2(el)} start="6:30" end="18:00" step="15" format={24} />
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Stunden: <b className='hourTextField'>{workhours2.toFixed(2)}</b></h6>
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Überstunden: <b className='hourTextField'>{Math.round((workhours2 - 7.8) * 100) / 100}</b></h6>
         </div>
 
-        <div className="row-span-2 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </div>
-      </div>
-
-      <div className='grid grid-rows-3 grid-flow-col gap-4'>
-        <div className="row-span-3 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </div>
-
-        <div className="row-span-3 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <div id='Mittwoch' className="row-span-3 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Mittwoch</h5>
+            <TimePicker value={timecome3} onChange={el => settimecome3(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
+            <TimePicker value={timegehen3} onChange={el => settimegehen3(el)} start="6:30" end="18:00" step="15" format={24} />
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Stunden: <b className='hourTextField'>{workhours3.toFixed(2)}</b></h6>
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Überstunden: <b className='hourTextField'>{Math.round((workhours3 - 7.8) * 100) / 100}</b></h6>
         </div>
       </div>
       
-
-        <Container>
-          <Row>
-          <Card id='Montag' style={{ width: viewM +"rem", margin: '0.5rem' }}>
-            <Card.Body>
-              <Card.Title className='textcenter'>Montag</Card.Title>
-            <TimePicker value={timecome1} onChange={el => settimecome1(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
-            <TimePicker value={timegehen1} onChange={el => settimegehen1(el)} start="6:30" end="18:00" step="15" format={24} />
-            <Card.Text>Stunden:<br />
-              <h4 className = 'hourTextField'>{workhours1.toFixed(2)}</h4>
-              Überstunden: <h4 className = 'hourTextField'>{Math.round((workhours1 - 7.8) * 100) / 100}</h4>
-            </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card id='Dienstag' style={{ width: viewM +"rem", margin: '0.5rem' }}>
-            <Card.Body>
-              <Card.Title className='textcenter'>Dienstag</Card.Title>
-            <TimePicker value={timecome2} onChange={el => settimecome2(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
-            <TimePicker value={timegehen2} onChange={el => settimegehen2(el)} start="6:30" end="18:00" step="15" format={24} />
-            <Card.Text>Stunden:<br />
-              <h4 className = 'hourTextField'>{workhours2.toFixed(2)}</h4>
-              Überstunden: <h4 className = 'hourTextField'>{Math.round((workhours2 - 7.8) * 100) / 100}</h4>
-            </Card.Text>  
-            </Card.Body>
-          </Card>
-          <Card id='Mittwoch' style={{ width: viewM +"rem", margin: '0.5rem' }}>
-            <Card.Body>
-              <Card.Title className='textcenter'>Mittwoch</Card.Title>
-            <TimePicker value={timecome3} onChange={el => settimecome3(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
-            <TimePicker value={timegehen3} onChange={el => settimegehen3(el)} start="6:30" end="18:00" step="15" format={24} />
-            <Card.Text>Stunden:<br />
-              <h4 className = 'hourTextField'>{workhours3.toFixed(2)}</h4>
-              Überstunden: <h4 className = 'hourTextField'>{Math.round((workhours3 - 7.8) * 100) / 100}</h4>
-            </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card id='Donnerstag' style={{ width: viewM +"rem", margin: '0.5rem' }}>
-            <Card.Body>
-              <Card.Title className='textcenter'>Donnerstag</Card.Title>
+      <div className='grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-2 pt-2 lg:pb-5'>
+        <div id="Donnerstag" className="row-span-3 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Donnerstag</h5>
             <TimePicker value={timecome4} onChange={el => settimecome4(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
             <TimePicker value={timegehen4} onChange={el => settimegehen4(el)} start="6:30" end="18:00" step="15" format={24} />
-            <Card.Text>Stunden:<br />
-              <h4 className = 'hourTextField'>{workhours4.toFixed(2)}</h4>
-              Überstunden: <h4 className = 'hourTextField'>{Math.round((workhours4 - 7.8) * 100) / 100}</h4>
-            </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card id='Freitag' style={{ width: viewM +"rem", margin: '0.5rem' }}>
-            <Card.Body>
-              <Card.Title className='textcenter'>Freitag</Card.Title>
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Stunden: <b className='hourTextField'>{workhours4.toFixed(2)}</b></h6>
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Überstunden: <b className='hourTextField'>{Math.round((workhours4 - 7.8) * 100) / 100}</b></h6>
+        </div>
+
+        <div id='Freitag' className="row-span-3 max-w-sm p-6  border border-gray-200 rounded-lg shadow-md">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Freitag</h5>
             <TimePicker value={timecome5} onChange={el => settimecome5(el)} start="6:30" end="18:00" step="15" format={24} style={{ marginBottom: '0.5rem' }} />
             <TimePicker value={timegehen5} onChange={el => settimegehen5(el)} start="6:30" end="18:00" step="15" format={24} />
-            <Card.Text>Stunden:<br />
-              <h4 className = 'hourTextField'>{workhours5.toFixed(2)}</h4>
-              Überstunden: <h4 className = 'hourTextField'>{Math.round((workhours5 - 7.8) * 100) / 100}</h4>
-            </Card.Text>
-            </Card.Body>
-          </Card>
-          </Row> <br/>
-          <h3>Wochenstunden Kontingent: {wochenstunden} Std.</h3>
-          <Button className='highlight-button'
-          style={{ display: 'block', margin: '0 auto' }}
-            onClick={() => {
-              settimecome1(23400);
-              settimegehen1(53280);
-              settimecome2(23400);
-              settimegehen2(53280);
-              settimecome3(23400);
-              settimegehen3(53280);
-              settimecome4(23400);
-              settimegehen4(53280);
-              settimecome5(23400);
-              settimegehen5(53280);
-            }}
-          >
-            Zurücksetzen
-          </Button>
-        </Container>
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Stunden: <b className='hourTextField'>{workhours5.toFixed(2)}</b></h6>
+            <h6 className="font-normal text-xl text-gray-700 dark:text-gray-400">Überstunden: <b className='hourTextField'>{Math.round((workhours5 - 7.8) * 100) / 100}</b></h6>
+        </div>
+      </div>
+      <h4 className="mt-5 font-bold text-4xl md:text-2xl mb-4 text-center">Wochenstunden Kontingent: <b className='hourTextField'>{wochenstunden} Std.</b> <br/><br/>
+        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded object-center" onClick={() => {
+                settimecome1(23400);
+                settimegehen1(53280);
+                settimecome2(23400);
+                settimegehen2(53280);
+                settimecome3(23400);
+                settimegehen3(53280);
+                settimecome4(23400);
+                settimegehen4(53280);
+                settimecome5(23400);
+                settimegehen5(53280);
+              }}>Zurücksetzen</button>
+      </h4>
 
       <footer className="cntr-footer">
           Created with  <a href='Cat.mp4'>❤ </a>  by Marlon Gehrmann and Julian Maier 
