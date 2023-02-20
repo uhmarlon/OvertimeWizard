9a8ad447ae3378ac99aa0b3a94116f96974ea611
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2023-02-20 11:25:49 +0100

    🧨
    scheiß white mode weg damit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,14 +28,14 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <div className="text-black dark:text-white flex flex-row justify-center w-full h-full bg-gradient-to-bl from-white to-[#fff] dark:from-black dark:to-[#001429] min-h-screen">
+      <div className="text-white flex flex-row justify-center w-full h-full bg-gradient-to-bl from-black to-[#001429] min-h-screen">
         <div className="w-[80%] md:w-[45rem]">
           <Component {...pageProps} />
           </div>
       </div>
       <div
         ref={ballCanvas}
-        className="ball-transitions pointer-events-none fixed z-30 h-6 w-6 rounded-full border border-black bg-transparent opacity-0 shadow-md duration-200 dark:border-white" 
+        className="ball-transitions pointer-events-none fixed z-30 h-6 w-6 rounded-full border bg-transparent opacity-0 shadow-md duration-200 border-white" 
       />
       <Analytics />
     </>
